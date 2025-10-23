7f45b51c924e32235c823414b707be3b38c45346
jojoCesar <jonathan.cesar@epitech.eu>
2025-10-23 10:32:25 +0200

    main_test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include <Arduino.h>
+#include "../lib/gestionLed.h"
+#include "../lib/config.h"
+#include "DS1307.h"
 
-#include <Arduino.h>
+// Déclaration des objets et prototypes
+extern void couleurLed(uint8_t modeActuel);
+extern void initclock();
+extern void printTime();
+extern void modeConfiguration();
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  // Initialisation de l’horloge RTC
+  initclock();
+  // Affichage de l’heure au démarrage
+  printTime();
+  // Initialisation du mode LED standard
+  couleurLed(MODE_STANDARD);
+  // Passage en mode configuration pour test
+  modeConfiguration();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(50);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(50);
+  // Clignotement simple de la LED en mode maintenance
+  couleurLed(MODE_MAINTENANCE);
+  delay(1000);
+
+  // Affichage régulier de l’heure
+  printTime();
+  delay(2000);
+
+  // Passage en mode économique
+  couleurLed(MODE_ECONOMIQUE);
+  delay(1000);
 }
+
